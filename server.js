c8d81f7b88e1b3a0fb8274fc763e2d0b016845b6
Julie Wang <julie.wang@emory.edu>
2020-02-22 15:13:51 -0500

    change res

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from App Engine!');
+  //res.send('Hello from App Engine!');
+  res.sendFile(path.join(__dirname + '/home.html'));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
