087b5d07c7adb4296a73f5cb470b0e53aa4b5fdb
Julie Wang <julie.wang@emory.edu>
2020-02-22 15:31:22 -0500

    connected css

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path');
 app.get('/', (req, res) => {
   //res.send('Hello from App Engine!');
   res.sendFile(path.join(__dirname + '/home.html'));
+  res.sendFile(path.join(__dirname + '/style.css'));
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
