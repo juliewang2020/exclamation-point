fa3773cae7f67a87018b39872c90fc3af8ae6dc4
Julie Wang <julie.wang@emory.edu>
2020-02-22 15:42:27 -0500

    add routes

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const path = require('path');
 
 app.get('/', (req, res) => {
   //res.send('Hello from App Engine!');
-  res.sendFile(path.join(__dirname + 'public/home.html'));
+  res.sendFile(path.join(__dirname + '/public/home.html'));
 });
 
 app.get('/style.css', function(req, res) {
-  res.sendFile(__dirname + "public/style.css");
+  res.sendFile(__dirname + "/public/style.css");
 });
 
 // Listen to the App Engine-specified port, or 8080 otherwise
