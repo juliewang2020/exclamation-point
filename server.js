523ee56bed7188819f67ff80d1c41363432cb939
hallowpeno <chang.emily98@gmail.com>
2020-02-23 02:17:03 -0500

    asdfasdfasdfasdfasdf

--- server.js
+++ server.js
@@ -91,12 +91,12 @@ app.get('/homeHeader.png', function(req, res) {
 
 app.post('/', function(req, res) {
   console.log(req);
-  // var name = req.body.name;
-  // var item = req.body.item;
-  // var phone = req.body.phone;
-  // var location = req.body.location;
-  // var specRequest = req.body.specRequest;
-  var mes = "Hey Period Pals! " + req.body;
+  var name = req.body.name;
+  var item = req.body.item;
+  var phone = req.body.phone;
+  var location = req.body.location;
+  var specRequest = req.body.specRequest;
+  var mes = "Hey Period Pals! " + name + " needs a " + item + "!";
   textAlert(mes);
   console.log("sent message");
   res.sendStatus(200);
